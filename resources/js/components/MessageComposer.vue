7ab9d23fee7b4ad726383a12d3778038cc2a56f5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-19 20:21:23 +0100

    add emojy picker

--- resources/js/components/MessageComposer.vue
+++ resources/js/components/MessageComposer.vue
@@ -1,5 +1,9 @@
 <template>
+    <div id="emojyPicker"></div>
     <div class="composer input-group mb-3">
+        <button class="btn border d-flex justify-content-center align-items-center" @click="pickEmojy">
+            <box-icon name='smile'></box-icon>
+        </button>
         <textarea type="text" class="form-control" placeholder="Type your message here..." v-model="message"
                   @keydown.enter="send" rows="1" style="resize: none"></textarea>
         <button class="btn btn-primary d-flex align-items-center justify-content-center" type="button" @click="send">
@@ -9,13 +13,15 @@
 </template>
 
 <script>
-import 'boxicons'
+import 'boxicons';
+import {createPicker} from 'picmo';
 
 export default {
     name: "MessageComposer",
     data() {
         return {
             message: '',
+            toggle_emojy: false,
         }
     },
     methods: {
@@ -26,8 +32,23 @@ export default {
             }
             this.$emit('send', this.message);
             this.message = '';
+        },
+        pickEmojy() {
+            const rootElement = document.querySelector('#emojyPicker');
+            if (this.toggle_emojy) {
+                rootElement.innerHTML = '';
+                this.toggle_emojy = false;
+                return;
+            }
+            const picker = createPicker({rootElement});
+            this.toggle_emojy = true;
+
+            // The picker emits an event when an emoji is selected. Do with it as you will!
+            picker.addEventListener('emoji:select', event => {
+                this.message += event.emoji;
+            });
         }
-    }
+    },
 }
 </script>
 
