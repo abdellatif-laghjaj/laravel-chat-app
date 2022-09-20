8a11deecefb5deaace30c5143d28ad9649ba3119
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-20 16:10:54 +0100

    update contact header

--- resources/js/components/Conversation.vue
+++ resources/js/components/Conversation.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="conversation w-100">
         <div class="card">
-            <div class="card-header bg-primary text-white">
-                {{ contact ? contact.name : "Abdel ChatX" }}
+            <div class="card-header bg-primary text-white d-flex align-items-center gap-4">
+                <img :src="'storage/profile_images/' + contact.profile_image" :alt="contact.name"
+                     class="rounded-circle contact-image" v-if="contact">
+                <div class="contact-details d-flex flex-column">
+                    <span class="contact-name">{{ contact ? contact.name : "Abdel ChatX" }}</span>
+                    <span class="contact-phone">{{ contact ? contact.phone : '' }}</span>
+                </div>
             </div>
             <div class="card-body pb-0">
                 <MessagesFeed :contact="contact" :messages="messages"/>
@@ -48,14 +53,25 @@ export default {
         MessageComposer
     },
     mounted() {
+        console.log(this.contact);
         console.log('Component mounted.')
     }
 }
 </script>
 
 <style lang="scss" scoped>
+$contact_image_width: 50px;
 .card-header {
     font-weight: bold;
     font-size: 18px;
+}
+
+.contact-image {
+    width: $contact_image_width;
+}
+
+.contact-phone {
+    font-size: 12px;
+    opacity: 0.8;
 }
 </style>
